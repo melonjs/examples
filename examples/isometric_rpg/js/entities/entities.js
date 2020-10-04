6f873e4358bf189b3b968bf8bc3f559ba9be56ee
Olivier Biot <olivier.biot@me.com>
2020-10-04 09:30:58 +0800

    [#1] fix isometric RPG animation not working

--- examples/isometric_rpg/js/entities/entities.js
+++ examples/isometric_rpg/js/entities/entities.js
@@ -31,9 +31,12 @@ game.PlayerEntity = me.Sprite.extend({
         me.input.bindKey(me.input.KEY.DOWN,  "down");
 
         // define an additional basic walking animation
-        this.addAnimation("walk", [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        this.addAnimation("walk_left",  [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+        this.addAnimation("walk_right", [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]);
+        this.addAnimation("walk_up",    [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]);
+        this.addAnimation("walk_down",  [0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11]);
         // set default one
-        this.setCurrentAnimation("walk");
+        this.setCurrentAnimation("walk_down");
     },
 
     /* -----
@@ -46,18 +49,30 @@ game.PlayerEntity = me.Sprite.extend({
         if (me.input.isKeyPressed("left")) {
             // update the entity velocity
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            if (!this.isCurrentAnimation("walk_left")) {
+                this.setCurrentAnimation("walk_left");
+            }
         } else if (me.input.isKeyPressed("right")) {
             // update the entity velocity
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            if (!this.isCurrentAnimation("walk_right")) {
+                this.setCurrentAnimation("walk_right");
+            }
         } else {
             this.body.vel.x = 0;
         }
         if (me.input.isKeyPressed("up")) {
             // update the entity velocity
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
+            if (!this.isCurrentAnimation("walk_up")) {
+                this.setCurrentAnimation("walk_up");
+            }
         } else if (me.input.isKeyPressed("down")) {
             // update the entity velocity
             this.body.vel.y += this.body.accel.y * me.timer.tick;
+            if (!this.isCurrentAnimation("walk_down")) {
+                this.setCurrentAnimation("walk_down");
+            }
         } else {
             this.body.vel.y = 0;
         }
@@ -70,7 +85,7 @@ game.PlayerEntity = me.Sprite.extend({
 
         // check if we moved (an "idle" animation would definitely be cleaner)
         if (this.body.vel.x !== 0 || this.body.vel.y !== 0) {
-            this._super(me.Entity, "update", [dt]);
+            this._super(me.Sprite, "update", [dt]);
             return true;
         }
     },
