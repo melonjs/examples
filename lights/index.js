2973dd15d1aaf8843cf0542fd624eb4355f7debf
Olivier Biot <olivier.biot@me.com>
2022-06-07 09:02:52 +0800

    add the new light2d effect

---
+++ lights/index.js
@@ -0,0 +1,66 @@
+import * as me from 'https://esm.run/melonjs@10.10';
+
+class PlayScreen extends me.Stage {
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent() {
+
+        // background sprite
+        var bg_sprite = new me.Sprite(
+            me.game.viewport.width / 2,
+            me.game.viewport.height / 2, {
+                image : "background",
+                anchorPoint : {x:0.5, y:0.5}
+        });
+        bg_sprite.scale(2);
+
+        // add to the game world
+        me.game.world.addChild(bg_sprite);
+
+        // stage light system
+        var whiteLight = new me.Light2d(
+            me.game.viewport.width / 2,
+            me.game.viewport.height / 2,
+            140,
+            "#fff",
+            0.7
+        );
+
+        // darker ambient light
+        this.ambientLight.parseCSS("#1117");
+        // spot light
+        this.lights.set("whiteLight", whiteLight);
+
+        // light follow the mouse
+        me.input.registerPointerEvent("pointermove", me.game.viewport, (event) => {
+            whiteLight.centerOn(event.gameX, event.gameY);
+        });
+    }
+};
+
+/**
+ *
+ * Initialize the application
+ */
+export default function onload() {
+
+    // Initialize the video.
+    if (!me.video.init(728, 410, {parent : "screen", scaleMethod : "flex", renderer : me.video.WEBGL})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+    }
+
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new PlayScreen());
+
+    me.loader.preload([{
+        name   : "background",
+        type   : "image",
+        src    : "data/pixel-art-16-bit-sega-streets-of-rage-city-wallpaper.jpg",
+    }],
+        () => {
+            me.state.change(me.state.PLAY);
+        }
+    );
+}
