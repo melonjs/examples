740f6125c1c46b1ca66693e835d1ecae4458efc5
Olivier Biot <olivier.biot@me.com>
2023-01-05 16:04:54 +0800

    add an infinite desert example

--- tiled_example_loader/js/resources.js
+++ tiled_example_loader/js/resources.js
@@ -9,6 +9,7 @@ var resources = [
     { name: "free_tileset_version_10",      type: "image",  src: "data/map/free_tileset_version_10.png" },
     // desert example
     { name: "desert",                       type: "tmx",    src: "data/map/desert.tmx" },
+    { name: "desert-infinite",              type: "tmx",    src: "data/map/desert-infinite.tmx" },
     { name: "desert",                       type: "tsx",    src: "data/map/desert.tsx" },
     { name: "tmw_desert_spacing",           type: "image",  src: "data/map/tmw_desert_spacing.png" },
     // sewer
