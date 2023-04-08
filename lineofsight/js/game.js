382365ca1d5d018a787e996cddb224d96cc87960
Olivier Biot <olivier.biot@me.com>
2023-04-08 15:59:22 +0800

    convert the line of sight source code to proper es6 structure

--- lineofsight/js/game.js
+++ lineofsight/js/game.js
@@ -3,21 +3,17 @@ import * as me from 'https://esm.run/melonjs';
 import PlayScreen from './screens/play.js';
 
 /* Game namespace */
-var game = {
     // Run on page load.
-    onload : function () {
-        // Initialize the video.
-        if (!me.video.init(800, 600, {parent : "screen", scale : "auto", renderer : me.video.CANVAS})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // set the "Play/Ingame" Screen Object
-        me.state.set(me.state.PLAY, new PlayScreen());
-
-        // switch to PLAY state
-        me.state.change(me.state.PLAY);
+export default function onload() {
+    // Initialize the video.
+    if (!me.video.init(800, 600, {parent : "screen", scale : "auto", renderer : me.video.CANVAS})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
     }
-};
 
-export default game;
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new PlayScreen());
+
+    // switch to PLAY state
+    me.state.change(me.state.PLAY);
+}
